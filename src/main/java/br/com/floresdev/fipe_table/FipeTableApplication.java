064e43143adb5ab1c9bc0ab8cc4ef1dc1b3a074e
Miguel Fernando Flores Barreto <miguel.barreto@arqdigital.com.br>
2024-08-21 16:20:05 -0300

    Initial commit

---
+++ src/main/java/br/com/floresdev/fipe_table/FipeTableApplication.java
@@ -0,0 +1,13 @@
+package br.com.floresdev.fipe_table;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FipeTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FipeTableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/floresdev/fipe_table/FipeTableApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.floresdev.fipe_table;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FipeTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
